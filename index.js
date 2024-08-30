b96dfd6f52bb079519f989140a9ff4e38b99ba83
perosofic420 <115893984+perosofic420@users.noreply.github.com>
2024-08-31 00:27:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,27 @@ const path = require('path');
 const express = require('./src/express');
 require('./src/mysql');
 
-function GetApiFiles() {
-    const apiDir = path.join(__dirname, 'src', 'api');
-    const apiFolders = fs.readdirSync(apiDir);
-    for (const folder of apiFolders) {
-        const folderPath = path.join(apiDir, folder);
-        const files = fs.readdirSync(folderPath);
-        for (const file of files) {
-            const filePath = path.join(folderPath, file);
-            const { method, url, execute } = require(filePath);
-            switch (method) {
-            case "GET":
-                express.get(url, execute);
-                break;
-            case "POST":
-                express.post(url, execute);
-                break;
-            case "DELETE":
-                express.delete(url, execute);
-                break;
-            }
+const apiDir = path.join(__dirname, 'src', 'api');
+const apiFolders = fs.readdirSync(apiDir);
+for (const folder of apiFolders) {
+    const folderPath = path.join(apiDir, folder);
+    const files = fs.readdirSync(folderPath);
+    for (const file of files) {
+        const filePath = path.join(folderPath, file);
+        const { method, url, execute } = require(filePath);
+        switch (method) {
+        case "GET":
+            express.get(url, execute);
+            break;
+        case "POST":
+            express.post(url, execute);
+            break;
+        case "DELETE":
+            express.delete(url, execute);
+            break;
         }
     }
 }
-
-GetApiFiles();
 
 express.listen(3000);
 
